08695cd6a590367f3de45246f735a97f9afd84f5
Mohamed <midou95liverpool@gmail.com>
2022-12-14 18:45:29 +0100

    Patient Added
    I created the logic for adding a new patients

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -2,10 +2,95 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Patient;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 class PatientController extends Controller
 {
-    Public function index()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        return view('patient.index');
+        //
+          $patients = Patient::all();
+        //dd($patients);
+        return view('patient.index', compact("patients"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //dd($request->all());
+        $patient = new Patient();
+
+        $patient->nom = request('nom');
+        $patient->prenom = request('prenom');
+        $patient->age = request('age');
+
+        $patient->save();
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Patient  $patient
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Patient $patient)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Patient  $patient
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Patient $patient)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Patient  $patient
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Patient $patient)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Patient  $patient
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Patient $patient)
+    {
+        //
     }
 }
--- resources/views/patient/index.blade.php
+++ resources/views/patient/index.blade.php
@@ -46,7 +46,31 @@
                     </tr>
                 </thead>
                 <tbody class="table-border-bottom-0">
-
+                  @foreach ($patients as $patient)
+                      <tr>
+                          <th scope="row">{{ $patient->id }}</th>
+                          <td>{{ $patient->nom }}</td>
+                          <td>{{ $patient->prenom }}</td>
+                          <td>{{ $patient->age }}</td>
+                          <td>
+                              <div class="dropdown">
+                                  <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
+                                      data-bs-toggle="dropdown" aria-expanded="false">
+                                      <i class="bx bx-dots-vertical-rounded"></i>
+                                  </button>
+                                  <div class="dropdown-menu" style="">
+                                      <a class="dropdown-item" href=""><i
+                                              class="bx bx-show me-1"></i> Show</a>
+                                      <a class="dropdown-item" href=""><i
+                                              class="bx bx-edit-alt me-1"></i> Edit</a>
+                                      <a class="dropdown-item" href=""><i
+                                              class="bx bx-trash me-1"></i>
+                                          Delete</a>
+                                  </div>
+                              </div>
+                          </td>
+                      </tr>
+                  @endforeach
                 </tbody>
             </table>
         </div>
