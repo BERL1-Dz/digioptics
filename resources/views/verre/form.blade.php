32b30f1eb9cf53de490a5030e17562f58539fb17
unknown <samiskoubi@gmail.com>
2022-12-23 18:39:24 +0100

    multiple CSS fix

--- resources/views/verre/form.blade.php
+++ resources/views/verre/form.blade.php
@@ -1,93 +1,69 @@
 <div class="row">
-  Code Fournisseur:
-  <select name="code_fournisseur" class="form-control">
-    @foreach($fournisseurs as $fournisseur)
-      <option value="{{$fournisseur->id}}">{{$fournisseur->code_fournisseur}}</option>
-    @endforeach
-    </select>
+    <div class="col">
+        <label for="emailWithTitle" class="form-label">Code Fournisseur:</label>
+        <select name="code_fournisseur" class="form-control">
+            @foreach ($fournisseurs as $fournisseur)
+                <option value="{{ $fournisseur->id }}">{{ $fournisseur->code_fournisseur }}</option>
+            @endforeach
+        </select>
+    </div>
 </div>
 
 <div class="row">
-<div class="col mb-0">
-    <label for="emailWithTitle" class="form-label"> Code verre</label>
-    <input type="text" id="emailWithTitle" class="form-control"
-    placeholder="048700"
-    name="code_verre"
-    />
-</div>
+    <div class="col mt-1">
+        <label for="emailWithTitle" class="form-label">Code verre:</label>
+        <input type="text" id="emailWithTitle" class="form-control" placeholder="048700" name="code_verre" />
+    </div>
 
 </div>
 <div class="row">
-<div class="col mb-0">
-    <label for="emailWithTitle" class="form-label"> Verre Indic</label>
-    <input type="text" id="emailWithTitle" class="form-control"
-    placeholder="1.67"
-    required
-    name="index_verre"/>
-</div>
+    <div class="col mt-1">
+        <label for="emailWithTitle" class="form-label">Verre Indic:</label>
+        <input type="text" id="emailWithTitle" class="form-control" placeholder="1.67" required name="index_verre" />
+    </div>
 
 </div>
-<div class="row g-2">
-    <div class="col mb-0">
-        <label for="emailWithTitle" class="form-label">Material</label>
-        <input type="text" id="emailWithTitle" class="form-control"
-            placeholder="Organique"
-            required
-            name="material"/>
+<div class="row mt-1 g-2">
+    <div class="col mt-1">
+        <label for="emailWithTitle" class="form-label">Material:</label>
+        <input type="text" id="emailWithTitle" class="form-control" placeholder="Organique" required
+            name="material" />
     </div>
-    <div class="col mb-0">
-        <label for="dobWithTitle" class="form-label">Diametre</label>
-        <input type="number" id="dobWithTitle" class="form-control"
-        placeholder="60 Ø"
-        required
-        name="diametre"/>
+    <div class="col mt-1">
+        <label for="dobWithTitle" class="form-label">Diametre:</label>
+        <input type="number" id="dobWithTitle" class="form-control" placeholder="60 Ø" required name="diametre" />
     </div>
 </div>
-<div class="row g-2">
-    <div class="col mb-0">
+<div class="row mt-1 g-2">
+    <div class="col mt-1">
         <label for="emailWithTitle" class="form-label">Surface</label>
-        <input type="text" id="emailWithTitle" class="form-control"
-        placeholder="Surface"
-        required
-        name="surface"/>
+        <input type="text" id="emailWithTitle" class="form-control" placeholder="Surface" required name="surface" />
     </div>
-    <div class="col mb-0">
-        <label for="dobWithTitle" class="form-label">Sphére</label>
-        <input type="text" id="dobWithTitle" class="form-control"
-            placeholder="Sph"
-            name="sph"/>
+    <div class="col mt-1">
+        <label for="dobWithTitle" class="form-label">Sphére:</label>
+        <input type="text" id="dobWithTitle" class="form-control" placeholder="Sph" name="sph" />
     </div>
-    <div class="col mb-0">
-        <label for="dobWithTitle" class="form-label">Clynder</label>
-        <input type="text" id="dobWithTitle" class="form-control"
-            placeholder="Cly"
-            name="cly" />
+    <div class="col mt-1">
+        <label for="dobWithTitle" class="form-label">Clynder:</label>
+        <input type="text" id="dobWithTitle" class="form-control" placeholder="Cly" name="cly" />
     </div>
 </div>
 
-<div class="row g-2">
-<div class="col mb-0">
-    <label for="emailWithTitle" class="form-label">Option</label>
-    <input type="text" id="emailWithTitle" class="form-control"
-        placeholder="Anti-Réflt"
-        required
-        name="option"/>
-</div>
+<div class="row mt-1 g-2">
+    <div class="col mt-1">
+        <label for="emailWithTitle" class="form-label">Option:</label>
+        <input type="text" id="emailWithTitle" class="form-control" placeholder="Anti-Réflt" required
+            name="option" />
+    </div>
 </div>
 
-<div class="row g-2">
-    <div class="col mb-0">
+<div class="row mt-1 g-2">
+    <div class="col mt-1">
         <label for="dobWithTitle" class="form-label">Pa_Verre</label>
-        <input type="number" id="dobWithTitle" class="form-control"
-            placeholder="Pa_Verre"
-            required
-            name="pa_verre"/>
+        <input type="number" id="dobWithTitle" class="form-control" placeholder="Pa_Verre" required name="pa_verre" />
     </div>
-    <div class="col mb-0">
+    <div class="col mt-1">
         <label for="dobWithTitle" class="form-label">Pv_Verre</label>
-        <input type="number" id="dobWithTitle" class="form-control"
-            placeholder="Pv_Verre"
-            required
-            name="pv_verre"/>
+        <input type="number" id="dobWithTitle" class="form-control" placeholder="Pv_Verre" required name="pv_verre" />
     </div>
 </div>
--- resources/views/verre/index.blade.php
+++ resources/views/verre/index.blade.php
@@ -3,22 +3,22 @@
 @section('content')
     <div class="container-xxl flex-grow-1 container-p-y">
         <!-- Vertically Centered Modal -->
-        <div class="mt-3">
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalCenter">
-                Ajouté Un verre
-            </button>
 
-            <!-- Modal -->
-            <div class="modal fade" id="modalCenter" tabindex="-1" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="modalCenterTitle">Modal title</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <form action="{{ url('create_verre') }}" method="post" enctype="multipart/form-data">
-                          @csrf
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalCenter">
+            Ajouter un verre
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="modalCenter" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalCenterTitle">Formulaire Verre</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form action="{{ url('create_verre') }}" method="post" enctype="multipart/form-data">
+                        @csrf
                         <div class="modal-body">
                             @include('verre.form')
                         </div>
@@ -28,63 +28,64 @@
                             </button>
                             <button type="submit" class="btn btn-primary">Save changes</button>
                         </div>
-                        </form>
+                    </form>
 
-                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="table-responsive text-nowrap">
-        <table class="table card-table">
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Code</th>
-                    <th>Fournisseur</th>
-                    <th>Indic</th>
-                    <th>Material</th>
-                    <th>Diametre</th>
-                    <th>Surface</th>
-                    <th>SPH</th>
-                    <th>CLY</th>
-                    <th>Option</th>
-                    <th>Prix d'achat </th>
-                    <th>Prix de vents</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody class="table-border-bottom-0">
-                @foreach ($verres as $verre)
+        <div class="table-responsive text-nowrap mt-3">
+            <table class="table card-table">
+                <thead>
                     <tr>
-                        <th scope="row">{{ $verre->id }}</th>
-                        <th scope="row">{{ $verre->code_verre }}</th>
-                        <th scope="row">{{ $verre->fournisseur->nom}}</th>
-                        <th scope="row">{{ $verre->index_verre }}</th>
-                        <th scope="row">{{ $verre->material }}</th>
-                        <th scope="row">{{ $verre->diametre }}</th>
-                        <th scope="row">{{ $verre->surface }}</th>
-                        <th scope="row">{{ $verre->sph }}</th>
-                        <th scope="row">{{ $verre->cly }}</th>
-                        <th scope="row">{{ $verre->option }}</th>
-                        <th scope="row">{{ $verre->pa_verre }}</th>
-                        <th scope="row">{{ $verre->pv_verre }}</th>
-                        <td>
-                            <div class="dropdown">
-                                <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    <i class="bx bx-dots-vertical-rounded"></i>
-                                </button>
-                                <div class="dropdown-menu" style="">
-                                    <a class="dropdown-item" href=""><i class="bx bx-show me-1"></i> Show</a>
-                                    <a class="dropdown-item" href="verreEdit/{{ $verre['id'] }}"><i class="bx bx-edit-alt me-1"></i> Edit</a>
-                                    <a class="dropdown-item" href="verre/delete/{{ $verre['id'] }}"><i class="bx bx-trash me-1"></i>Delete</a>
-                                </div>
-                            </div>
-                        </td>
+                        <th>Id</th>
+                        <th>Code</th>
+                        <th>Fournisseur</th>
+                        <th>Indic</th>
+                        <th>Material</th>
+                        <th>Diametre</th>
+                        <th>Surface</th>
+                        <th>SPH</th>
+                        <th>CLY</th>
+                        <th>Option</th>
+                        <th>Prix d'achat </th>
+                        <th>Prix de vents</th>
+                        <th>Actions</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody class="table-border-bottom-0">
+                    @foreach ($verres as $verre)
+                        <tr>
+                            <th scope="row">{{ $verre->id }}</th>
+                            <th scope="row">{{ $verre->code_verre }}</th>
+                            <th scope="row">{{ $verre->fournisseur->nom }}</th>
+                            <th scope="row">{{ $verre->index_verre }}</th>
+                            <th scope="row">{{ $verre->material }}</th>
+                            <th scope="row">{{ $verre->diametre }}</th>
+                            <th scope="row">{{ $verre->surface }}</th>
+                            <th scope="row">{{ $verre->sph }}</th>
+                            <th scope="row">{{ $verre->cly }}</th>
+                            <th scope="row">{{ $verre->option }}</th>
+                            <th scope="row">{{ $verre->pa_verre }}</th>
+                            <th scope="row">{{ $verre->pv_verre }}</th>
+                            <td>
+                                <div class="dropdown">
+                                    <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                        <i class="bx bx-dots-vertical-rounded"></i>
+                                    </button>
+                                    <div class="dropdown-menu" style="">
+                                        <a class="dropdown-item" href=""><i class="bx bx-show me-1"></i> Show</a>
+                                        <a class="dropdown-item" href="verreEdit/{{ $verre['id'] }}"><i
+                                                class="bx bx-edit-alt me-1"></i> Edit</a>
+                                        <a class="dropdown-item" href="verre/delete/{{ $verre['id'] }}"><i
+                                                class="bx bx-trash me-1"></i>Delete</a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
