07c25be38d02b9c5430a6086c5e38ff93d60d01e
unknown <samiskoubi@gmail.com>
2022-12-25 14:18:37 +0100

    correction show part 3

--- resources/views/correction/show.blade.php
+++ resources/views/correction/show.blade.php
@@ -216,61 +216,166 @@
                         </div>
                     </div>
 
+                    <div class="d-flex " style="border: 1px solid #000">
+                        <div class="d-flex-column w-100 ">
+                            <div class="p-2 text-align-center"
+                                style="border: 1px solid #000; text-align:center; font-weight:bold;font-size:18px">Loin
+                            </div>
+                            <div class="p-2 "style="border: 1px solid #000">
+                                <ul>
+                                    <Li>OD : Hmc</Li>
+                                    <Li>OG : Hmc</Li>
+                                    <Li>Monture : Silhouette</Li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="d-flex-column flex-shrink-1" style=" width: 25%;">
+
+                            <div class="p-2 "
+                                style="border: 1px solid #000; text-align:center; font-weight:bold; font-size:18px">Prix
+                            </div>
+                            <div class="p-2 "style="border: 1px solid #000">
+                                <ul>
+                                    <Li>800.00</Li>
+                                    <Li>800.00</Li>
+                                    <Li>7000.00 </Li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="d-flex " style="border: 1px solid #000">
+                        <div class="d-flex-column w-100 ">
+                            <div class="p-2 text-align-center"
+                                style="border: 1px solid #000; text-align:center; font-weight:bold;font-size:18px">Pres
+                            </div>
+                            <div class="p-2 "style="border: 1px solid #000">
+                                <ul>
+                                    <Li>OD : Hmc</Li>
+                                    <Li>OG : Hmc</Li>
+                                    <Li>Monture : Silhouette</Li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="d-flex-column flex-shrink-1" style=" width: 25%;">
+
+                            <div class="p-2 "
+                                style="border: 1px solid #000; text-align:center; font-weight:bold; font-size:18px">Prix
+                            </div>
+                            <div class="p-2 "style="border: 1px solid #000">
+                                <ul>
+                                    <Li>800.00</Li>
+                                    <Li>800.00</Li>
+                                    <Li>7000.00 </Li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="d-flex " style="border: 1px solid #000">
+                        <div class="d-flex-column w-100 ">
+                            <div class="p-2 text-align-center"
+                                style="border: 1px solid #000; text-align:center; font-weight:bold;font-size:18px">
+                                Accessoires
+                            </div>
+                            <div class="p-2 "style="border: 1px solid #000">
+                                <ul>
+                                    <Li>Accessoire 1: X</Li>
+                                    <Li>Accessoire 1: X</Li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="d-flex-column flex-shrink-1" style=" width: 25%;">
+
+                            <div class="p-2 "
+                                style="border: 1px solid #000; text-align:center; font-weight:bold; font-size:18px">Prix
+                            </div>
+                            <div class="p-2 "style="border: 1px solid #000">
+                                <ul>
+                                    <Li>000.00</Li>
+                                    <Li>000.00</Li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="d-flex " style="border: 1px solid #000">
+                        <div class="d-flex w-100 ">
+                            <div class="p-2 flex-fill bd-highlight"
+                                style="border: 1px solid #000; text-align:center; font-weight:bold;font-size:18px">RAP :
+                                4500.00DA</div>
+                            <div class="p-2 flex-fill bd-highlight"
+                                style="border: 1px solid #000; text-align:center; font-weight:bold;font-size:18px">REM :
+                                100.00DA</div>
+                            <div class="p-2 flex-fill bd-highlight"
+                                style="border: 1px solid #000;text-align:center; font-weight:bold;font-size:18px">Encaisse
+                                : 4000.00DA
+                            </div>
+                            <div class="d-flex-column flex-shrink-1" style=" width: 20%;">
+
+                                <div class="p-2 "
+                                    style="border: 1px solid #000; text-align:center; font-weight:bold; font-size:18px">
+                                    Total : 8600.00Da
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <style type="text/css">
-        .cell-round {
-            height: 49px;
-            background-color: #f4f9ff00;
-            color: #3a4850;
-            font: 700 14px/17px Roboto, Arial, sans-serif;
-            padding: 15.5px 14.5px;
-            text-align: left;
+        <style type="text/css">
+            .cell-round {
+                height: 49px;
+                background-color: #f4f9ff00;
+                color: #3a4850;
+                font: 700 14px/17px Roboto, Arial, sans-serif;
+                padding: 15.5px 14.5px;
+                text-align: left;
 
-        }
+            }
 
-        .cell {
-            background-color: #f7f7f700;
-            border-top: 1px solid #000000;
-            color: #3a4850;
-            font: 700 14px/17px Roboto, Arial, sans-serif;
-            padding: 15.5px 14.5px;
-            text-align: left;
-        }
+            .cell {
+                background-color: #f7f7f700;
+                border-top: 1px solid #000000;
+                color: #3a4850;
+                font: 700 14px/17px Roboto, Arial, sans-serif;
+                padding: 15.5px 14.5px;
+                text-align: left;
+            }
 
-        .drop {
-            border-top: 1px solid #000000;
-            /* border-bottom: 1px solid #000000; */
-            color: #6b6b6b;
-            font: 400 14px/17px Roboto, Arial, sans-serif;
-            height: 49px;
-            position: relative;
-            white-space: nowrap;
-            width: 100%;
-        }
+            .drop {
+                border-top: 1px solid #000000;
+                /* border-bottom: 1px solid #000000; */
+                color: #6b6b6b;
+                font: 400 14px/17px Roboto, Arial, sans-serif;
+                height: 49px;
+                position: relative;
+                white-space: nowrap;
+                width: 100%;
+            }
 
-        .tab {
-            border-left: 1px solid #000000;
-            border-right: 1px solid #000000;
-            border-bottom: 1px solid #000000;
-            width: 90px;
-        }
+            .tab {
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px solid #000000;
+                width: 90px;
+            }
 
-        .form-select-custom {
-            border: 0px solid #000000 !important;
-            border-radius: 0rem !important;
-        }
+            .form-select-custom {
+                border: 0px solid #000000 !important;
+                border-radius: 0rem !important;
+            }
 
-        .cellmin1 {
-            font-weight: bold;
-        }
+            .cellmin1 {
+                font-weight: bold;
+            }
 
-        .cellmin2 {
-            width: 90px;
-            border: 1px solid;
-        }
-    </style>
-@endsection
+            .cellmin2 {
+                width: 90px;
+                border: 1px solid;
+            }
+        </style>
+    @endsection
