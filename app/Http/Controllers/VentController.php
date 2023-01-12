5cbd1656b5d537439c563559b7ef5cf1c86e341c
Mohamed <midou95liverpool@gmail.com>
2023-01-12 11:06:56 +0100

    new model/controller/migration

---
+++ app/Http/Controllers/VentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vent;
+use Illuminate\Http\Request;
+
+class VentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Vent  $vent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vent $vent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Vent  $vent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vent $vent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Vent  $vent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Vent $vent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Vent  $vent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vent $vent)
+    {
+        //
+    }
+}
